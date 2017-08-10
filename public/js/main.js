04048eb599a8864cf49eea1e09902f9724f0e182
davo <davo.grindel@gmail.com>
2017-08-10 02:06:50 -0300

    second

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,12 @@ function openContainer(div){
 	
 	
 }
+
+function enviar(){
+	name = $("input[name=name]").val();
+	email = $("input[name=email]").val();
+	title = $("input[name=title]").val();
+	comment = $("#comment").val();
+	
+	alert(name + " "+ email + " "+ title + " " + comment);
+}
